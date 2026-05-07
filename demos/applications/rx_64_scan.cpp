183596d05bb14a23c7346ed21d2b73d522ed1b4f
MaliaLabor <malia.labor@hotmail.com>
2026-05-07 14:55:47 -0700

    clean up demo

--- demos/applications/rx_64_scan.cpp
+++ demos/applications/rx_64_scan.cpp
@@ -44,51 +44,27 @@ void application()
 
   auto servo = hal::actuator::rx_64(uart, servo_config, clock);
 
-  // std::array<hal::hertz, 9> available_bauds = { 9600,   19200,  57600,
-  //                                               115200, 200000, 250000,
-  //                                               400000, 500000, 1000000 };
-  [[maybe_unused]] bool device_found = false;
-  [[maybe_unused]] bool baud_rate_found = false;
+  std::array<hal::hertz, 9> available_bauds = { 9600,   19200,  57600,
+                                                115200, 200000, 250000,
+                                                400000, 500000, 1000000 };
+  bool baud_rate_found = false;
   // loop through bauds
-  // for (auto current_baud : available_bauds) {
-  uart->configure({ .baud_rate = 57600 });
-  // loop through addresses
-  // for (uint8_t i = 0; i < 254; i++) {
-  //   // ping with address i, result into device_found
-  //   hal::print<32>(*console, "Checking ID: %d\n", i);
-  //   device_found = servo.ping_id(i);
-  //   if (device_found) {
-  //     baud_rate_found = true;
-  //     hal::print<32>(*console,
-  //                    "Servo found with baud rate %f at id: %d\n",
-  //                    current_baud,
-  //                    i);
-  //     servo.set_id(i);
-  //     // turn on led to id which servo has id reported
-  //     servo.led_toggle(true);
-  //     hal::delay(*clock, 3s);
-  //     servo.led_toggle(false);
-  //   }
-  // }
-
-  hal::print<32>(*console, "\nSending Pings on baud %f : ", 57600);
-  for (uint8_t i = 0; i < 254; i++) {
-    // ping with address i, result into device_found
-    hal::print<32>(*console, "%d", i);
-    servo.ping_id(i);
-    if (i != 253) {
-      hal::print(*console, ", ");
+  for (auto current_baud : available_bauds) {
+    uart->configure({ .baud_rate = current_baud });
+    hal::print<48>(*console, "\nSending Pings on baud %f : ", current_baud);
+    for (uint8_t i = 0; i < 254; i++) {
+      hal::print(*console, ".");
+      servo.ping_id(i);
+      hal::delay(*clock, 10ms);
     }
-    hal::delay(*clock, 100ms);
-  }
-  uint8_t id = servo.read_serial();
-  if (id != 254) {
-    hal::print<32>(*console, "\nID found: %d\n", id);
-    baud_rate_found = true;
-  }
+    uint8_t id = servo.read_serial();
+    if (id != 254) {
+      hal::print<64>(*console, "\nID found: %d \n", id);
+      baud_rate_found = true;
+    }
 
-  // if (baud_rate_found) {
-  //   break;
-  // }
-  // }
+    if (baud_rate_found) {
+      break;
+    }
+  }
 }
